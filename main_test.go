1b1b56f66f380fd222d775c94658aaaf406e5a77
Hefezopf <markus@hopf-it.de>
2020-05-16 16:41:35 +0200

    test

--- main_test.go
+++ main_test.go
@@ -4,16 +4,24 @@ import (
   "testing"
   "math"
 )
+var pA Point = Point{
+  X: 1,
+  Y: 1,
+}
+var pB Point = Point{
+  X: 1,
+  Y: 3,
+}
+var pC Point = Point{
+  X: 4,
+  Y: 4,
+}
+var pD Point = Point{
+  X: 6,
+  Y: 1,
+}
 
 func TestDetermineMinimumAB(t *testing.T) {
-	pA := Point{
-		X: 1,
-		Y: 1,
-	}
-	pB := Point{
-		X: 1,
-		Y: 3,
-	}
 	ans := DetermineMinimum(pA, pB)
 	if ans != 2 {
 		t.Errorf("DetermineMinimum()= %f; want 2", ans)
@@ -21,44 +29,13 @@ func TestDetermineMinimumAB(t *testing.T) {
 }
 
 func TestDetermineMinimumAC(t *testing.T) {
-	pA := Point{
-		X: 1,
-		Y: 1,
-	}
-	pC := Point{
-		X: 4,
-		Y: 4,
-  }
   ans := DetermineMinimum(pA, pC)
 	if math.Round(ans) != 4.0 {
 		t.Errorf("DetermineMinimum()= %f; want 4.242641", ans)
 	}
 }
 
-const float64EqualityThreshold = 1e-9
-
-func almostEqual(a, b float64) bool {
-    return math.Abs(a - b) > float64EqualityThreshold
-}
-
 func TestDetermineMinimumOfArray(t *testing.T) {
-	pA := Point{
-		X: 1,
-		Y: 1,
-	}
-	pB := Point{
-		X: 1,
-		Y: 3,
-	}
-	pC := Point{
-		X: 4,
-		Y: 4,
-	}
-	pD := Point{
-		X: 6,
-		Y: 1,
-	}
-
 	ans := DetermineMinimumOfArray(pA, pB, pC, pD)
 	if ans != 2 {
 		t.Errorf("DetermineMinimum()= %f; want 2", ans)
